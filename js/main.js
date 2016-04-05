efb1e50057510d3ac4b3dd85a8b22b51779e9d20
vtruong5 <vtruong5@masonlive.gmu.edu>
2016-04-04 22:17:06 -0400

    Add Items and item functions
    Still need character stats

--- js/main.js
+++ js/main.js
@@ -13,125 +13,315 @@ window.onload = function () {
     
     "use strict";
     
-    var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
+    var game = new Phaser.Game(500 , 400, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
     
     
     function preload() {
 
-        game.load.image('sky', 'assets/bg.png');
-        game.load.image('ground', 'assets/platform.png');     
-        game.load.image('player', 'assets/miner.png');
-        game.load.spritesheet('veggies', 'assets/underground.png', 32, 32);
-        game.load.audio('beep', 'assets/sound1.wav');
+        game.load.tilemap('map', 'assets/test.json', null, Phaser.Tilemap.TILED_JSON);
+        game.load.image('tile1', 'assets/tile1.png');
+        game.load.image('tile2', 'assets/tile2.png');
+        game.load.image('player', 'assets/miner_small.png');
+        game.load.image('rock', 'assets/bigrock.png');
+        game.load.image('dirt', 'assets/dirt.png');
+        game.load.image('chest', 'assets/collect.png');
+        game.load.spritesheet('chestAni', 'assets/Heal6.png', 100, 100, 30);
+        //game.load.spritesheet('treasure', 'assets/gems.png', 32, 32, 5);
+        game.load.image('treasure', 'assets/gem.png');
+        game.load.image('bat', 'assets/enemy.png');
+        game.load.image('food', 'assets/food.png');
+        game.load.image('map', 'assets/map_piece.png');
+        game.load.image('final', 'assets/end_treasure.png');
+        
     }
     
-    var sprite;
-    var group;
+    var map;
+    var tileset;
+    var layer;
+    var p;
     var cursors;
-    var platforms;
-    var collect = 0;
-    var sound;
+    var key1;    
+    
+    //text
+    var message;
+    var location;
+    var hungerText;
+    var attentionText;
+    var mapText;
+    
+    //objects
+    var ground;
+    var rocks;
+    var chests;
+    var treasures;
+    var enemies;
+    var foods;
+    var map;
+    var endPrize;
+    //var endX = game.rnd.between(0, 99)*32;
+    //var endY = game.rnd.between(0, 99)*32;
+    var endX = 1650;
+    var endY = 1650;
+    
+    //animations
+    var addAni;
+    var animate;    
+    
+    //in game stats
+    var mapPieceCount = 0;
 
+    
     function create() {
-
+        
         game.physics.startSystem(Phaser.Physics.ARCADE);
+        game.stage.backgroundColor = '#000000';
         
-        //background
-        game.add.sprite(0, 0, 'sky');
+        //make world
+        map = game.add.tilemap('map');
+        map.addTilesetImage('tile1', 'tile1');
+        map.addTilesetImage('tile2', 'tile2');       
+        layer = map.createLayer('Tile Layer 1');
+        layer.resizeWorld();
         
-        //sound
-        sound = game.add.audio('beep');
+        //make stuff
         
-        //platforms
-        //  The platforms group contains the ground and the 2 ledges we can jump on
-        platforms = game.add.group();
-
-        //  We will enable physics for any object that is created in this group
-        platforms.enableBody = true;
-
-        // Here we create the ground.
-        var ground = platforms.create(0, game.world.height, 'ground');
-
-        //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
-        ground.scale.setTo(2, 2);
-
-        //  This stops it from falling away when you jump on it
-        ground.body.immovable = true;
-
-        //  Now let's create ledges
-        var ledge = platforms.create(-50, 75, 'ground');
-        ledge.body.immovable = true;
-        ledge = platforms.create(450, 75, 'ground');
-        ledge.body.immovable = true;
-
-        //player
-        sprite = game.add.sprite(200, 35, 'player');
-        game.physics.arcade.enable(sprite);
+        //endgame prize
+        endPrize = game.add.physicsGroup();
+        var end = endPrize.create(endX, endY, 'final');  
+        endPrize.visible = false;
+        
+        //add map pieces
+        map = game.add.physicsGroup();
+        //var piece = map.create(game.rnd.between(0, 50)*32,game.rnd.between(0, 50)*32, 'map');
+        //piece = map.create(game.rnd.between(49, 99)*32,game.rnd.between(0, 50)*32, 'map');
+        //piece = map.create(game.rnd.between(0, 50)*32,game.rnd.between(49, 99)*32, 'map');
+        //piece = map.create(game.rnd.between(49, 99)*32,game.rnd.between(49, 99)*32, 'map');
  
-        //items
-            group = game.add.physicsGroup();
-            for (var i = 0; i < 10; i++)
-            {
-                var c = group.create(game.rnd.between(0, 770), game.rnd.between(300, 570), 'veggies', 36);
-            }
-            for (var i = 0; i < 500; i++)
-            {
-                var c = group.create(game.rnd.between(0, 770), game.rnd.between(100, 570), 'veggies', game.rnd.between(0, 35));
+        var piece = map.create(1500,1500, 'map');
+        piece = map.create(1550, 1500, 'map');
+        piece = map.create(1600, 1500, 'map');
+        piece = map.create(1650, 1500, 'map');
+        
+        //add food
+        foods = game.add.physicsGroup();
+        for(var i = 0; i < 5; i++){
+            var f = foods.create(game.rnd.between(0, 99)*32,game.rnd.between(0, 99)*32, 'food');
+        }        
+        var f = foods.create(1700,1700, 'food');
+        
+        //add tresure
+        treasures = game.add.physicsGroup();
+        for (var i = 0; i < 100; i++){
+            for (var j = 0; j < 100; j++){
+                var rnd = game.rnd.between(0, 100)
+                if(rnd < 10){                
+                    var t = treasures.create(32*i,j*32, 'treasure');
+                }
+              }
+        }           
+        
+        //treasure chest
+        chests = game.add.physicsGroup();
+        for (var i = 0; i < 100; i++){
+            for (var j = 0; j < 100; j++){
+                var rnd = game.rnd.between(0, 100)
+                if(rnd == 5){                
+                    var c = chests.create(32*i,j*32, 'chest');
+                }
+              }
+        }  
 
-                c.body.mass = -100;
-            }
+        //create dirt
+        ground = game.add.group();
+        for (var i = 0; i < 100; i++){
+            for (var j = 0; j < 100; j++){
+                var rnd = game.rnd.between(0, 100)
+                if(rnd > 10){                
+                    var d = ground.create(32*i,j*32, 'dirt');
+                    game.physics.arcade.enable(d);
+                }
+              }
+        }        
+         
+        //make rock   
+        rocks = game.add.physicsGroup();
+        for (var i = 0; i < 100; i++){
+            for (var j = 0; j < 100; j++){
+                var rnd = game.rnd.between(0, 100)
+                if(rnd > 70){
+                    var r = rocks.create(32*i,j*32, 'rock');
+                    r.body.mass = -100;                   
+                }
 
-            cursors = game.input.keyboard.createCursorKeys();
+            }
+        } 
+        
+        //make enemies
+        var y = 2400
+        enemies = game.add.physicsGroup();
+        for(var i = 0; i < 5; i++){
+            var e = enemies.create(game.world.randomX, y, 'bat');
+            e.body.velocity.x = game.rnd.between(200, 400);
+            y += 128;
+        }
+        
+        //make player
+        p = game.add.sprite(game.world.centerX, game.world.centerY, 'player');
+        game.physics.enable(p);     
+        p.body.collideWorldBounds = true;
+        
+        //game camera
+        game.camera.follow(p);     
+        
+        //controls
+        cursors = game.input.keyboard.createCursorKeys();
+        key1 = game.input.keyboard.addKey(Phaser.Keyboard.Z);
+        
+        //text
+        message = game.add.text(10, 375, 'move with arrow keys and collect with [ Z ]', { fontSize: '10px', fill: '#fff' });
+        message.fontSize = 15;
+        message.font = 'Arial Black';
+        message.fixedToCamera = true;
+        
+        location = game.add.text(10, 10, 'X: ' + p.x + ' Y: ' + p.y, { fontSize: '10px', fill: '#fff' });
+        location.fontSize = 15;
+        location.font = 'Arial Black';
+        location.fixedToCamera = true;
+        
+        mapText = game.add.text(360, 10, 'Map Pieces: ' + mapPieceCount + '/4', { fontSize: '10px', fill: '#fff' });
+        mapText.fontSize = 15;
+        mapText.font = 'Arial Black';
+        mapText.fixedToCamera = true;        
+
+        hungerText = game.add.text(10, 30, 'hunger', { fontSize: '10px', fill: '#fff' });
+        hungerText.fontSize = 15;
+        hungerText.font = 'Arial Black';
+        hungerText.fixedToCamera = true;    
+
+        attentionText = game.add.text(10, 50, 'attention', { fontSize: '10px', fill: '#fff' });
+        attentionText.fontSize = 15;
+        attentionText.font = 'Arial Black';
+        attentionText.fixedToCamera = true;         
     }
 
     function update() {
-        if (game.physics.arcade.collide(sprite, group, collisionHandler, processHandler, this))
-                {
-                    console.log('boom');
-                }
-        game.physics.arcade.collide(sprite, platforms);
-        sprite.body.velocity.x = 0;
-        sprite.body.velocity.y = 0;
+        
+        game.physics.arcade.collide(p, layer);
+        game.physics.arcade.collide(p, ground, groundCollision, null, this);
+        game.physics.arcade.overlap(p, chests, itemCollision, null, this);
+        game.physics.arcade.overlap(p, treasures, treasureCollision, null, this);        
+        game.physics.arcade.collide(p, ground);
+        game.physics.arcade.collide(p, rocks);
+        enemies.forEach(checkPos, this);
+        game.physics.arcade.overlap(p, enemies, enemyCollision, null, this);
+        game.physics.arcade.overlap(p, foods, foodCollision, null, this);
+        game.physics.arcade.overlap(p, map, mapCollision, null, this);
+        
+        if(mapPieceCount == 4){
+            game.physics.arcade.overlap(p, endPrize, endCollision, null, this);
+        }
+
+        
+        p.body.velocity.x = 0;
+        p.body.velocity.y = 0;
         if (cursors.left.isDown)
         {
-            sprite.body.velocity.x = -200;
+            p.body.velocity.x = -200;
         }
         else if (cursors.right.isDown)
         {
-            sprite.body.velocity.x = 200;
+            p.body.velocity.x = 200;
         }
 
         if (cursors.up.isDown)
         {
-            sprite.body.velocity.y = -200;
+            p.body.velocity.y = -200;
         }
         else if (cursors.down.isDown)
         {
-            sprite.body.velocity.y = 200;
+            p.body.velocity.y = 200;
+        }
+        
+
+        location.text = 'X: ' + Math.floor(p.x) + ' Y: ' + Math.floor(p.y);
+    }
+    //kill dirt
+    function groundCollision (o1, o2) {
+        o2.kill();
+    }
+
+    //flying dirts
+    /*
+    function checkPos (dirt) {
+        if (dirt.x > 3200 || dirt.x < 0 || dirt.y > 3200 || dirt.y < 0)
+        {
+            dirt.kill();
+            message.text = 'dirt kill';
         }
 
+    }
+    */
+    
+   function itemCollision (o1, o2) {
+       if(key1.isDown){
+            addAni = game.add.sprite(o2.x-40, o2.y-25, 'chestAni');                
+            animate = addAni.animations.add('chestAction');
+            addAni.animations.play('chestAction', 50, false);        
+            o2.kill();     
+            message.text = 'get chest';
+       }
+    }   
 
-        if (collect >= 10){
-            game.add.text(16, 16, 'Congratulations!! You\'re Rich!!', { font: '20px Arial', fill: '#ffffff' });
+    function checkPos (enemy) {
+        if (enemy.x > 3200)
+        {
+            enemy.x = -100;
         }
- 
+
+    }    
+    
+    function treasureCollision (o1, o2){
+        if (key1.isDown)
+        {        
+            o2.kill();     
+            message.text = 'get treasure';
+        }
     }
     
-    function processHandler (player, veg) {
-
-        return true;
-
+    function enemyCollision(p, e){
+        //player gets more hungry
+        //score affected
+        message.text = 'ouch';
     }
-
-    function collisionHandler (player, veg) {
-
-        if (veg.frame == 36)
+    
+    function foodCollision(p, f){
+        if (key1.isDown)
         {
-            veg.kill();
-            sound.play();
-            collect = collect + 1;
-        }
-
-    }
-
+            f.kill();
+            message.text = 'eat food';
+        }        
+    }  
+    
+    function mapCollision(p, piece){
+        if (key1.isDown)
+        {
+            piece.kill();
+            message.text = 'found map piece';
+            mapPieceCount++;
+            mapText.text = 'Map Pieces: ' + mapPieceCount + '/4';
+            
+            if(mapPieceCount == 4){
+                mapText.text = 'X:' + endX + ' Y:' + endY;
+                endPrize.visible = true;
+            }
+        }        
+    }    
+    
+    function endCollision(p, end){
+        if (key1.isDown)
+        {
+            end.kill();
+            message.text = 'END';
+        }        
+    }  
 };
